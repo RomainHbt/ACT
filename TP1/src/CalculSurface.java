4800f95f390b9688d2fb767362279bf69c3f1582
Hembert Romain <hembert@a13p21.fil.univ-lille1.fr>
2016-09-22 09:05:33 +0200

    Lecture d'un fichier

--- TP1/src/CalculSurface.java
+++ TP1/src/CalculSurface.java
@@ -1,8 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 public class CalculSurface {
 
 	public static void main(String[] args) {
 		
+		String chaine="";
+		String fichier ="./donneesPourTests/ex100_5980";
+		
+		//lecture du fichier texte	
+		try{
+			InputStream ips=new FileInputStream(fichier); 
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String ligne;
+			while ((ligne=br.readLine())!=null){
+				chaine+=ligne+"\n";
+			}
+			br.close(); 
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+		
+		System.out.println(chaine);
 
 	}
 
