d726ae492328c35ba2c9b6d5df17bbe97c5ec8e6
Hembert Romain <romain.hembert@etudiant.univ-lille1.fr>
2016-09-29 09:13:15 +0200

    Algorithme fonctionnel

--- TP1/README.md
+++ TP1/README.md
@@ -1,5 +1,6 @@
 #TP1 - Diviser pour régner
 
+##Question 1
 Pseudo-code du calcul :
 
 * Entrées:
@@ -11,13 +12,26 @@ Pseudo-code du calcul :
     * un nombre correspondant à la surface maximale d'un rectangle du plan, vérifiant les contraintes
 
 * Code:
-  largeurMax = 0
-  Pour i allant de 0 à nbPoints :
-    abs1 = points[i].x
-    Pour j allant de i+1 à nbPoints :
-      abs2 = points[j].x
-      largeur = abs2 - abs1
-      Pour k allant de i+1 à j :
-        hauteur =  
+    surfaceMax = 0
+    Pour i allant de 0 à nbPoints-1 :
+        abs1 = points[i].x
+        Pour j allant de i+1 à nbPoints-1 :
+            abs2 = points[j].x
+            largeur = abs2 - abs1
+            hauteur = h
 
-  Affichage de largeurMax
+            Si j != i+1 : #Si vrai, il y a des points entre i et j
+                Pour k allant de i+1 à j-1 :
+                    Si points[k].y != 0 ET points[k].y < hauteur :
+                        hauteur = points[k].y
+                    FinSi
+                FinPour
+            FinSi
+
+            surface = largeur * hauteur
+            Si surface > surfaceMax
+                surfaceMax = surface
+            FinSi
+        FinPour
+    FinPour
+    Affichage de surfaceMax
--- TP1/src/CalculSurface.java
+++ TP1/src/CalculSurface.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 
 public class CalculSurface {
@@ -13,7 +14,8 @@ public class CalculSurface {
 
 	public static void main(String[] args) {
 		
-		String fichier ="./donneesPourTests/ex500_7616";
+		//String fichier ="./donneesPourTests/ex500_7616";
+		String fichier ="./donneesPourTests/ex10_24400144";
 		
 		//lecture du fichier texte	
 		try{
@@ -30,25 +32,54 @@ public class CalculSurface {
 			
 			ligne=br.readLine();
 			nbPoints = Long.parseLong(ligne);
+			points = new long[((int) nbPoints)+2][2];
 			
+			points[0][0] = 0;
+			points[0][1] = 0;
+			int i = 1;
 			while ((ligne=br.readLine())!=null){
-				
+				String[] coords = ligne.split(" ");
+				points[i][0] = Long.parseLong(coords[0]);
+				points[i][1] = Long.parseLong(coords[1]);
+				i++;
 			}
-			br.close(); 
+			br.close();
+			points[i][0] = l;
+			points[i][1] = 0;
+			//Arrays.sort(points);
+			
+			long res = calcul();
+			System.out.println(res);
 		}		
 		catch (Exception e){
-			System.out.println(e.toString());
+			System.err.println(e.toString());
 		}
 		
 
 	}
 	
-	public int calcul(){
-		/*
-		 * pour i allant de 0 à nbPoints
-		 */
-		
-		return 0;
+	public static long calcul(){
+		long surfaceMax = 0;
+		for (int i = 0; i < points.length; i++) {
+			long abs1 = points[i][0];
+			for (int j = i+1; j < points.length; j++) {
+				long abs2 = points[j][0];
+				long largeur = abs2 - abs1;
+				long hauteur = h;
+				long surface;
+
+				if(j != i+1){ // Points intermédiaires
+					for (int k = i+1; k < j; k++) {
+						if(points[k][1] != 0 && points[k][1] < hauteur) hauteur = points[k][1];
+					}
+				}
+				
+				surface = largeur * hauteur;
+				
+				if(surface > surfaceMax) surfaceMax = surface;
+			}
+		}
+		return surfaceMax;
 	}
 
 }
